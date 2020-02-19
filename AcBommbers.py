5e50ca8e2efaca84d986ec8350790a225f1bcd07
SpiderMIR <59828722+SpiderMIR@users.noreply.github.com>
2020-02-19 12:51:49 +0600

    Create AcBommbers.py

---
+++ AcBommbers.py
@@ -0,0 +1,11 @@
+git clone https://github.com/SpiderMIR/AcBommbers.git
+pkg install python2
+pkg install git
+apt update && apt upgrade
+git clone https://github.com/SpiderMIR/AcBommbers.git
+   
+   
+   
+   
+   
+   this is for termux
